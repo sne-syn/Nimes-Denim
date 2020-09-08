382f65f863329c8e747dd2b096a3ef416e89453f
sne-syn <snezhana.syngur@gmail.com>
2020-09-08 19:20:01 +0200

    new: Add homepage blocks styles

--- src/components/news/news.component.jsx
+++ src/components/news/news.component.jsx
@@ -18,10 +18,10 @@ const News = () => (
       </header>
       <div className="news__wrapper">
         <div className="news__text-column">
-          <h2 className="news__title">G- Star Updates Its Sustainable Collection</h2>
+          <h2 className="news__text-title">G- Star Updates Its Sustainable Collection</h2>
           <time className="news__time">Feb 12, 2020</time>
-          <p className="news__text">G-Star Raw is pushing the sustainability movement forward with a collection of stretch denim items for spring 2020 which is particularly designed for reuse. G-Star seeks to contribute to positive change in the denim industry, by using each sustainable innovation as a building block for a future. The brand has been working to recycle its jeans into new since 2012 when it released the first-ever Cradle to Cradle (C2C) Gold level-certified denim in late 2017.</p>
-          <a className="news__button" href={newsUrl}>Read more…</a>
+          <p className="news__text">G-Star Raw is pushing the sustainability movement forward with a collection of stretch denim items for spring 2020 which is particularly designed for reuse...</p>
+          <a className="news__button" href={newsUrl}>Read more …</a>
         </div>
         <div className="news__img-column">
           <img src={news} width={ImageDimensions.WIDTH} height={ImageDimensions.HEIGHT} alt="Collection stretch denim items for spring 2020" />
